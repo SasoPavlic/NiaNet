e98425d0cfc12fbcfc78d445c375972f4c2abe2b
SasoPavlic <saso.pavlic3@gmail.com>
2022-09-30 21:34:58 +0200

    Fixed version issues #10

--- tests/test_nianet.py
+++ tests/test_nianet.py
@@ -3,7 +3,7 @@ import torch
 
 
 def test_version():
-    assert __version__ == '1.0.0'
+    assert __version__ == '1.1.1'
 
 def test_gpu():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
